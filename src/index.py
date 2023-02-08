b5aab3d0dc282ebca8149c2df58b269dd593a994
brian2w <brian.wang900@gmail.com>
2023-02-09 00:50:03 +1100

    added boto3 dynamodb scan

--- src/index.py
+++ src/index.py
@@ -1,21 +1,31 @@
 import os
 from flask import Flask, request, render_template, url_for, json
+import boto3
 
 app = Flask(__name__)
+
+region = "ap-southeast-2"
+dynamodbTableName = "tickets"
+dynamodb = boto3.resource('dynamodb')
+ticketsDB = dynamodb.Table(dynamodbTableName)
+
+response = ticketsDB.scan()
+ticketList = response['Items']
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/getAllTickets", methods=['GET'])
-def getAllTickets():
-    filename = os.path.join(app.static_folder, 'Database', 'database.json')
-    with open(filename) as database:
-        data = json.load(database)
-    return data
+# @app.route("/getAllTickets", methods=['GET'])
+# def getAllTickets():
+#     filename = os.path.join(app.static_folder, 'Database', 'database.json')
+#     with open(filename) as database:
+#         data = json.load(database)
+#     return data
     
 
 @app.route("/sendTicket", methods=['POST'])
 def sendTicket():
     print(request.form['ticketName'])
+    # put ml algo here
     return "<p>Hello</p>"
