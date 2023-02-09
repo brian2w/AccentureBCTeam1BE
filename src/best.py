1b4c3c61cfb482c0a6047c6aa03e0b3d8ab0022b
Murtaza <m.ashraf@unswalumni.com>
2023-02-09 11:59:42 +1100

    function for specific ticket

--- src/best.py
+++ src/best.py
@@ -3,16 +3,17 @@ percentages = [76,45,99]
 id = 1 # id of ticket
 ticket_percent = {id: percentages}
 
+
 #### function to get average for specific ticket
 def get_average_of_scores(ticket_percent):
+    list_of_averages = []
     for id in ticket_percent: # loop through dictionary
         list_numbers = ticket_percent[id]
-
-
-    average = 0
-    average = sum(list_numbers) / len(list_numbers)
-    average = round(average,2)
-    print(average)
+        average = 0
+        average = sum(list_numbers) / len(list_numbers)
+        average = round(average,2)
+        list_of_averages.append(average)
+    return list_of_averages
 
 
 get_average_of_scores(ticket_percent) # function call
